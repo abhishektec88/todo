e0ae0fb06ceb60bd9c51c310eea24833a8a08904
abhishektec88 <68714056+abhishektec88@users.noreply.github.com>
2022-01-16 18:35:28 +0530

    Update TodoList.js

--- src/compnents/Home/TodoList.js
+++ src/compnents/Home/TodoList.js
@@ -1,20 +1,57 @@
 import React from "react";
-import { ListItem,TodoText} from "./Styled";
+import { ListItem, TodoText, TodoEdit, PButton } from "./Styled";
 
-export default function TodoList(props) {
-    const { name, done } = props.todo;
-    return (
+
+
+const TodoList = ({
+  todos,
+  completeTodo,
+  removeTodo,
+  editTodo,
+  editId,
+  handleEditChange,
+  inputValue,
+  setInputValue
+}) => {
+  return todos.map((todo) => (
+    <ListItem style={{ textDecoration: todo.isComplete ? "line-through" : "" }}>
+      {editId === todo.id && !todo.isComplete ? (
+        <TodoEdit 
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+      ) : (
         <>
-         <ListItem style={{ textDecoration: done ? "line-through" : "" }}>
-             <span className="span-check">
-             <input type="checkbox" onClick={props.completed}/>
-             </span>
-          <TodoText>{name}</TodoText>
-          <p type="button" onClick={props.remove}><i class="fas fa-trash-alt"></i></p>
-        </ListItem>   
+        <span className="span-check">
+            <input 
+            type="checkbox"
+            onClick={() => completeTodo(todo.id)} 
+            />
+        </span>
+        <TodoText>{todo.text}</TodoText>
+    </>
+      )}
+      {editId === todo.id && !todo.isComplete ? (
+        <span className="span-check">
+         <PButton onClick={() => editTodo(todo.id, inputValue)}>edit</PButton>
+        </span>
+      ) : (
+        <>
+          <p type="button" onClick={() => handleEditChange(todo.id, todo.text)} ><i className="fas fa-edit icon"></i></p>
+          <p type="button" onClick={() => removeTodo(todo.id)}><i className="fas fa-trash-alt icon"></i></p>
         </>
-    );
-}
+      )}
+    </ListItem>
+  ));
+};
+
+export default TodoList;
+
+
+
+
+
 
 
 
