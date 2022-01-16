3cafed9c45df6010047327cf8c5feaf18c321942
abhishektec88 <68714056+abhishektec88@users.noreply.github.com>
2022-01-16 18:36:29 +0530

    Update TodoForm.js

--- src/compnents/Home/TodoForm.js
+++ src/compnents/Home/TodoForm.js
@@ -1,16 +1,32 @@
-import React from "react";
-import { TodoInput, Form, AddButton } from "./Styled";
+import React,{useState} from "react";
+import { TodoInput, Form, PButton } from "./Styled";
 
 export default function TodoForm(props) {
+  const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+      complete: false
+    });
+    setInput("");
+  };
   return (
-    <Form  onSubmit={props.onSubmit} style={{ paddingLeft: 40, marginTop: 16 }}>
+    <Form  onSubmit={handleSubmit} style={{ paddingLeft: 40, marginTop: 16 }}>
       <TodoInput
         type="text"
-        value={props.value}
-        onChange={props.onChange}
+        value={input}
+        onChange={handleChange}
         placeholder="New Task"
       />
-      <AddButton type="submit">Add</AddButton>
+      <PButton type="submit">Add</PButton>
     </Form>
   );
 }
+
