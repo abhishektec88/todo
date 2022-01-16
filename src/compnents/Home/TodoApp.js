11e93c928c036d85bf97b74ba77a151cffeb135b
abhishektec88 <68714056+abhishektec88@users.noreply.github.com>
2022-01-16 18:37:11 +0530

    Update TodoApp.js

--- src/compnents/Home/TodoApp.js
+++ src/compnents/Home/TodoApp.js
@@ -1,65 +1,77 @@
-import React, { useState, useEffect,  } from "react";
+import React, { useState} from "react";
 
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 import { TodoTable } from "./Styled";
 
-//Initial tasks
-const tasks = [
-  { name: "task 1", done: false },
-  { name: "task 2", done: false },
-  { name: "task 3", done: false }
-];
+
 
 function TodoApp() {
-  const [todos, setTodos] = useState(tasks);
+  //Initial tasks
+  const [todos, setTodos] = useState([]);
+  const [editId, setEdit] = useState(false);
   const [inputValue, setInputValue] = useState("");
 
-  useEffect(() => {
-    let count = 0;
-    todos.map(todo => (!todo.done ? count++ : null));
-    document.title = `${count} task${count > 1 ? "s" : ""} todo`;
-  });
-
-  //
-  const _handleSubmit = e => {
-    e.preventDefault();
-    if (inputValue === "") return alert("Task name is required");
-
-    const newArr = todos.slice();
-    newArr.splice(0, 0, { name: inputValue, done: false });
-    setTodos(newArr);
-    setInputValue("");
+  const handleEditChange = (id, text) => {
+    setEdit(id);
+    setInputValue(text);
   };
 
-  const handleBntClick = ({ type, index }) => {
-    const newArr = todos.slice();
-    if (type === "remove") newArr.splice(index, 1);
-    else if (type === "completed") newArr[index].done = true;
-
-    return setTodos(newArr);
+  // Add the task
+  const addTodo = (todo) => {
+    const newTodos = [...todos, todo];
+    setTodos(newTodos);
   };
 
+  // Remove Task
+  const removeTodo = (id) => {
+    const removedArr = [...todos].filter((todoId) => todoId.id !== id);
+    setTodos(removedArr);
+  };
+
+  // Completed Task
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  //Edit Task
+  const editTodo = (id, text) => {
+    let editTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.text = text;
+      }
+      return todo;
+    });
+    setTodos(editTodos);
+    setEdit(false);
+  };
 
   return (
     <>
-      <TodoForm
-        onSubmit={_handleSubmit}
-        value={inputValue}
-        onChange={e => setInputValue(e.target.value)}
-      />
+      <TodoForm onSubmit={addTodo} />
       <TodoTable>
-        {todos.map((todo, index) => (
-          <TodoList
-            key={index}
-            todo={todo}
-            remove={() => handleBntClick({ type: "remove", index })}
-            completed={() => handleBntClick({ type: "completed", index })}
-          />
-        ))}
+      <TodoList
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        editTodo={editTodo}
+        handleEditChange={handleEditChange}
+        editId={editId}
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+      />
       </TodoTable>
     </>
   );
 }
 
+
+
 export default TodoApp;
+
